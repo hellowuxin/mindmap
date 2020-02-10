415ff0dd78c4ad76671d7ae0492c516ce8499c42
hellowuxin <hello_wuxin@qq.com>
2020-02-10 21:04:54 +0800

    显示

--- src/App.vue
+++ src/App.vue
@@ -28,6 +28,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  align-content: center;
+  justify-content: center;
 }
 </style>
