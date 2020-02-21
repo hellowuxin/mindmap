6f982633f9472e3805540b245cc22f6ec3a32d6f
hellowuxin <hello_wuxin@qq.com>
2020-02-21 16:58:09 +0800

    image loader

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   css: {
     extract: false
+  },
+  chainWebpack: config => {
+    config.module
+      .rule("images")
+      .use("url-loader")
+      .loader("url-loader")
+      .tap(options => Object.assign(options, { limit: Infinity }));
   }
 }
