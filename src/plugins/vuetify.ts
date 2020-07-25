ffa9806f574280a30c8d373f914cf9b451dfb995
hellowuxin <hello_wuxin@qq.com>
2020-07-25 11:42:24 +0800

    vue add ts

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,11 @@
+import '@mdi/font/css/materialdesignicons.css'
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  icons: {
+    iconfont: 'mdi',
+  },
+});
